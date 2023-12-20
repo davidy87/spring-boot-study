781a6aff0c712f9a3574ca48ddb27e5bd4251809
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-12-20 20:03:37 +0900

    8. 모니터링 메트릭 활용

---
+++ actuator/src/main/java/hello/order/OrderService.java
@@ -0,0 +1,13 @@
+package hello.order;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public interface OrderService {
+
+    void order();
+
+    void cancel();
+
+    AtomicInteger getStock();
+
+}
---
+++ actuator/src/main/java/hello/order/v4/OrderServiceV4.java
@@ -0,0 +1,46 @@
+package hello.order.v4;
+
+import hello.order.OrderService;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Timed("my.order")
+@RequiredArgsConstructor
+public class OrderServiceV4 implements OrderService {
+
+    private AtomicInteger stock = new AtomicInteger(100);
+
+    @Override
+    public void order() {
+        log.info("주문");
+        stock.incrementAndGet();
+        sleep(500);
+    }
+
+    @Override
+    public void cancel() {
+        log.info("취소");
+        stock.decrementAndGet();
+        sleep(200);
+    }
+
+    private static void sleep(int t) {
+        try {
+            Thread.sleep(t + new Random().nextInt(200));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+}
